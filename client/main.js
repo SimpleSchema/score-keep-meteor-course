ea0344f0ec4239e243ba58487973d11cff4d5e1c
Devon Mclean <dmclean154@gmail.com>
2018-08-21 18:05:28 -0700

    init commit

---
+++ client/main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
+import {Players, calculatePlayerPositions} from './../imports/api/players.js';
+import App from './../imports/ui/App.js';
+
+Meteor.startup(() => {
+
+  Tracker.autorun(() => {
+    let players = Players.find({}, {sort: {score: -1}}).fetch();
+    let positionedPlayers = calculatePlayerPositions(players);
+    let title = 'Score Keep';
+    let subTitle = 'Created by Devon McLean';
+    ReactDOM.render(<App title={title} players={positionedPlayers}/>, document.getElementById('app'));
+  });
+
+});
